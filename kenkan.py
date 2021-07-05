240b0896d80f90e2b16061d3c052dc7716119a2e
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-05 11:43:14 +0700

    Update kenkan.py

--- kenkan.py
+++ kenkan.py
@@ -6,6 +6,12 @@ import time
 import json
 import os
 from config import TOKEN
+import logging
+
+logging.basicConfig(filename="bot.log", format="%(asctime)s - %(levelname)s - %(message)s",
+                        datefmt="%Y/%m/%d %I:%M:%S %p", level=0)
+bot_url = "https://t.me/"+bot_username
+error_img = "https://image.freepik.com/free-vector/error-neon-signs-style-text_118419-3023.jpg"
 
 BOT = telebot.TeleBot(f"{TOKEN}")
 
